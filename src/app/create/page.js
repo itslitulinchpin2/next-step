0a9832c24336606abf82bc39d41b35894c5c13d3
itslitilinchpin2 <itslitulinchpin@gmail.com>
2023-10-23 00:58:44 +0200

    create implementation with cache:no-store

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,39 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 export default function Create(){
+    const router=useRouter();
     return(
-        <>Create!!</>
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            const title=event.target.title.value;
+            const body=event.target.body.value;
+            const options = {
+                method:'POST',
+                headers:{
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify({title,body})
+            }
+            fetch(`http://localhost:9999/topics`,options)
+            .then(res=>res.json())
+            .then(result=>{
+                console.log(result);
+                const lastId = result.id;
+                router.refresh()// 서버 컴포넌트를 강제로 다시 랜더링 하도록 하는 기능
+                router.push(`/read/${lastId}`)
+            })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title"></input>
+            </p>
+            <p>
+                <textarea  name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="create"></input>
+            </p>
+        
+        </form>
     )
 }
