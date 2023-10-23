4aebeefcb232363b646dcbbdb24ba3319cc96369
itslitilinchpin2 <itslitulinchpin@gmail.com>
2023-10-24 01:53:41 +0200

    CRUD - Delete

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,9 +1,10 @@
 "use client"
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 export function Control() {
   const params = useParams();
-
+  const router=useRouter();
   //console.log("파람스: ",params)
   const id = params.id
   return (
@@ -11,7 +12,28 @@ export function Control() {
       <li><Link href="/create">Create</Link></li>
       {id ? 
       <>      <li><Link href={`/update/${id}`}>Update</Link></li>
-      <li><input type="button" value="Delete"></input></li>
+              <li><input type="button" value="Delete"
+              onClick={()=>{
+
+                const options = {
+                  method:'DELETE',
+                  
+              }
+
+              fetch(`http://localhost:9999/topics/${id}`,options)
+              .then(res=>res.json())
+              .then(result=>{
+                  
+                  router.refresh()
+                  router.push(`/`)
+              })
+
+
+
+              }}
+              
+              
+              ></input></li>
       </>
 
       : <></>}
